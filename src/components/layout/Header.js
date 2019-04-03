436a7164f79532b8f1f45694b9b167d8a99073ff
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-04-03 21:49:46 +0000

    logout button linked to destroy session route in rails api, cookies not cleared on react side

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom"
 
-function Header(props){
-  return (
-    <header style={headerStyle}>
-      <h1 style={headerText}>Next Game</h1>
-      <Link style={linkStyle} to="/">Landing Page</Link> | 
-      <Link style={linkStyle} to="/preferences">Preferences</Link> | 
-      <Link style={linkStyle} to="/nextgames"> Next Games </Link> |
-      <Link style={linkStyle} to="/mygames"> MyGames </Link> |
-      <Link style={linkStyle} to="/signup"> Sign Up</Link> |
-      <Link style={linkStyle} to="/login"> Log In </Link>
-    </header>
-  )
+class Header extends Component {
+
+  handleLogout = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/logout',{
+      mode: 'cors', 
+      credentials: 'include',
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  render(){
+    return (
+      <header style={headerStyle}>
+        <h1 style={headerText}>Next Game</h1>
+        <button type="button" onClick ={this.handleLogout}className="btn btn-danger">Logout!</button>
+        <Link style={linkStyle} to="/">Landing Page</Link> | 
+        <Link style={linkStyle} to="/preferences">Preferences</Link> | 
+        <Link style={linkStyle} to="/nextgames"> Next Games </Link> |
+        <Link style={linkStyle} to="/mygames"> MyGames </Link> |
+        <Link style={linkStyle} to="/signup"> Sign Up</Link> |
+        <Link style={linkStyle} to="/login"> Log In </Link>
+      </header>
+    )
+  }
 }
 
 const headerStyle ={
