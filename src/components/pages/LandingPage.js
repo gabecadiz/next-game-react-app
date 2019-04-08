7f059739d981457ec98a4a6fffc940c246c3829d
Matt Cook <mcc1985@gmail.com>
2019-04-08 23:11:48 +0000

    Landing Page
    Installed react-pose library to have access to animations. Animated entrance of logo on landing page with simple slide-enter animation.

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,14 +1,62 @@
 import React, {Component} from 'react'
-import { Link } from "react-router-dom"
+import posed, { PoseGroup } from 'react-pose';
+import '../styles/LandingPage.css'
 
+
+const Logo = posed.div({
+  enter: {
+    x: 0,
+    opacity: 1,
+    delay: 300,
+    transition: {
+      y: { type: 'spring', stiffness: 1000, damping: 15 },
+      default: { duration: 2500 }
+    }
+  },
+  exit: {
+    x: 500,
+    opacity: 0,
+    transition: { duration: 1000 }
+  }
+});
+
+const NextGame = posed.div({
+  enter: { opacity: 1 },
+  exit: { opacity: 0 }
+});
 
 class LandingPage extends Component{
+
+  state = { isVisible: false };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        isVisible: true
+      });
+    }, 1000);
+  }
+
   render(){
+
+    const { isVisible } = this.state;
+
     return(
-      <div>
-        <h1>Hello Welcome to Next Game</h1>
-        <Link to="/signup"> <button type="button" className="btn btn-primary">Sign up</button></Link>  |
-        <Link to="/login"> <button type="button" className="btn btn-success">Log In</button> </Link>
+      <div className="landing-container">
+        <PoseGroup>
+          {isVisible && [
+            <NextGame key="next-game-title" className="landing-title">
+              <h1 key="land">Find <i>Your</i></h1>
+            </NextGame>
+          ]}
+        </PoseGroup>
+        <PoseGroup>
+          {isVisible && [
+              <Logo key="next-game-logo" className="landing-logo">
+                <img src="https://github.com/amilford87/next-game-react-app/blob/master/src/assets/next-game-logo.png?raw=true" alt="" />
+              </Logo>
+          ]}
+        </PoseGroup>
       </div>
     )
   }
